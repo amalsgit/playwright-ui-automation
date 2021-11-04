0fdabe720921c213cfdd32bc4024831f4c30801b
amal jayaraj <amaljayaraj07@gmail.com>
2021-11-04 15:22:46 +0100

    add test to check hooks

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,10 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const testConfig: PlaywrightTestConfig = {
   use: {
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'retain-on-failure',
+    video: 'retry-with-video',
+    trace: 'retry-with-trace',
   },
   reporter: process.env.CI ? [['github'], ['dot']] : 'html',
+  outputDir: './test-results',
+  retries: 1
 }
---
+++ tests/testHooks.spec.ts
@@ -0,0 +1,30 @@
+import test from '@playwright/test'
+import faker from 'faker'
+
+test.describe('Testing test hooks', () => {
+  let user: string
+
+  console.log('---------------INSIDE DESCRIBE-----------------')
+  test.beforeAll(() => {
+    console.log('---------------INSIDE BEFORE ALL-----------------')
+    console.log('Creating user')
+    user = `AwesomeUser ${faker.name.firstName()}`
+  })
+  test.afterAll(() => {
+    console.log('---------------INSIDE AFTER ALL-----------------')
+    console.log('Deleting User')
+    console.log(`User ${user} is deleted`)
+  })
+  test.beforeEach(() => {
+    console.log('---------------INSIDE BEFORE EACH-----------------')
+    console.log(`Login in as ${user}`)
+  })
+  test('Inside the 1st test', () => {
+    console.log('---------------INSIDE 1st TEST-----------------')
+    console.log(`Running test 1 as user ${user}`)
+  })
+  test('Inside the 2nd test', () => {
+    console.log('---------------INSIDE 2nd TEST-----------------')
+    console.log(`Running test 2 as user ${user}`)
+  })
+})
